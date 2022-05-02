62ea00d10f2038b65e6e329c5f7a2c41e71fd17d
Aluno <diogocardosop1956@gmail.com>
2022-05-02 16:06:37 -0300

    ja

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Academia</title>
+</head>
+<body>
+  <form action="" method="POST">
+      <P>
+         <label>E-mail</label>
+         <input type="text" name="email">
+</p>
+ <p>
+        <label>Senha</label>
+         <input type="password" name="senha">
+</P>
+  <p>
+        <button type="submit">Entrar</button>
+</P>
+
+</form>
+</body>
+</html>
